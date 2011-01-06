0849ad09fe142f05cd6c50775be9ddec9526982f
Steve Kondik <shade@chemlab.org>
2011-01-06 18:13:36 -0500

    Enable build with Android system.
     - Override Music app
     - Set package name to Music3
     - Configure analytics and aidl

--- Android.mk
+++ Android.mk
@@ -4,15 +4,24 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-	src/com/android/music/IMediaPlaybackService.aidl
+	src/org/abrantix/rockon/rockonnggl/IRockOnNextGenService.aidl
 
-LOCAL_PACKAGE_NAME := Music
+LOCAL_STATIC_JAVA_LIBRARIES := libGoogleAnalytics
 
-LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := Music3
 
 # LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_OVERRIDES_PACKAGES := Music
+
 include $(BUILD_PACKAGE)
+
+#######
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libGoogleAnalytics:libs/libGoogleAnalytics.jar
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
